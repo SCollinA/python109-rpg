6398bce5eb7f45eaecb05982b0327691e5bac0de
Collin Argo <collinargo@gmail.com>
2018-09-27 15:03:37 -0400

    give enemies bounties

--- rpg.py
+++ rpg.py
@@ -35,37 +35,11 @@ class Hero(Character):
             enemy.health -= self.power
         print("The %s does %d damage to the %s." % (self.name, self.power, enemy.name))
 
-class Goblin(Character):
+class HumanShield(Character):
     def __init__(self, name):
         self.name = name
-        self.health = 6
-        self.power = 2
-
-class Wizard(Character):
-    def __init__(self, name):
-        self.name = name
-        self.health = 6
-        self.power = 2
-    def attack(self, enemy):
-        # 10% of time Wizard attack is devastating
-        if random() > .9:
-            enemy.health -= self.power * 3
-        else:
-            enemy.health -= self.power
-
-# make a Zombie character that doesn't die even if his health is below zero
-class Zombie(Character):
-    def __init__(self, name):
-        self.name = name
-        self.health = 4
+        self.health = 20
         self.power = 1
-    def alive(self):
-        if self.health < 4:
-            self.eat_brains()
-        return True
-    def eat_brains(self):
-        print("brains...")
-        self.health = 4
 
 # make a new character called Medic that can sometimes recuperate 2 health points 
 # after being attacked with a probability of 20%
@@ -81,6 +55,42 @@ class Medic(Character):
     def recuperate(self):
         self.health += 2
 
+class Goblin(Character):
+    def __init__(self, name):
+        self.name = name
+        self.health = 6
+        self.power = 2
+        self.bounty = 5
+
+class Wizard(Character):
+    def __init__(self, name):
+        self.name = name
+        self.health = 6
+        self.power = 2
+        self.bounty = 6
+    def attack(self, enemy):
+        # 10% of time Wizard attack is devastating
+        if random() > .9:
+            enemy.health -= self.power * 3
+        else:
+            enemy.health -= self.power
+
+# make a Zombie character that doesn't die even if his health is below zero
+class Zombie(Character):
+    def __init__(self, name):
+        self.name = name
+        self.health = 4
+        self.power = 1
+        self.bounty = 10000
+    def alive(self):
+        if self.health < 4:
+            self.eat_brains()
+        return True
+    def eat_brains(self):
+        print("brains...")
+        self.health = 4
+
+
 # make a character called Shadow who has only 1 starting health 
 # but will only take damage about once out of every ten times he is attacked.
 class Shadow(Character):
@@ -88,17 +98,11 @@ class Shadow(Character):
         self.name = name
         self.health = 1
         self.power = 2
+        self.bounty = 2
     def alive(self):
         if random() > .1:
             self.health = 1
         return self.health > 0
-
-class HumanShield(Character):
-    def __init__(self, name):
-        self.name = name
-        self.health = 20
-        self.power = 1
-
 
 def main():
     hero = Hero("Hero")
